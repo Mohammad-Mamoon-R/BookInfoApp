082fe745debfe6409f5752f6d503deeabe5dedef
Mohammad Mamoon Roshangar <mohammad.roshangar@impetus.com>
2023-03-09 13:51:27 +0530

    new commit

--- src/main/java/com/rtb/ratings/RatingsServiceApplication.java
+++ src/main/java/com/rtb/ratings/RatingsServiceApplication.java
@@ -10,7 +10,5 @@ public class RatingsServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RatingsServiceApplication.class, args);
-		System.out.println("Main method");
 	}
-
 }
